3ccd6ad73370ffdbbaae7d1abe32d74a092b1ade
yaytse <arturmararash2000@gmail.com>
2026-09-07 16:15:12 -0600

    s

--- src/components/pages/Applications.jsx
+++ src/components/pages/Applications.jsx
@@ -13,38 +13,9 @@ function Applications() {
 
         <div className="applications-container">
 
-          <div className="application-card">
+          
 
-            <div className="application-card-top">
-                <span className="application-status">NOW OPEN</span>
-            </div>
 
-            <h3>First Year Rep Applications</h3>
-
-            <p className="due-date">
-                Application deadline: <strong>September 5th at 11:59 PM</strong>
-            </p>
-
-            <p className="description">
-                Calling all first years!
-
-                Want to get involved? Meet new people? Develop career-ready
-                skills? Here's your chance!
-
-                Apply to be a first year rep for the Engineering Students'
-                Society and be a part of Schulich history.
-            </p>
-
-            <a
-                href="https://forms.gle/82ewFgzuj77QBuZU8"
-                target="_blank"
-                rel="noreferrer"
-                className="application-btn"
-            >
-                Apply Now →
-            </a>
-
-          </div>
 
           <div className="application-card">
 
